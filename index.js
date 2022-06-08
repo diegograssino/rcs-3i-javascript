c3687aa3a1b0e75e03f72afdf15eb4b51af7c395
Diego Grassino <diegograssino@gmail.com>
2022-06-07 21:25:07 -0300

    Fin de la clase

---
+++ index.js
@@ -0,0 +1,251 @@
+// 2)
+
+// Dado un número, indicar si ese número es mayor a 10.
+
+// let numero = prompt("Ingrese su número");
+
+// if (numero > 10) {
+// 	alert("El número es mayor a 10");
+// };
+
+// Dado un número, indicar si ese número es menor o igual a 10.
+
+// let numero = prompt("Ingrese un número:");
+
+// if (numero <= 10) {
+// 	alert("El número es menor o igual a 10")
+// }
+
+// Dado un número, indicar si ese número es par.
+
+// let numero = prompt("Ingrese un número: ");
+
+// if (numero%2 === 0) {
+// 	alert("El número es par.")
+// }
+
+// Dado un número, indicar si ese número es impar.
+
+// let numero = prompt("Ingrese un número: ");
+
+// if (numero%2 !== 0) {5
+// 	alert("El número es impar.")
+// }
+
+// Ante un valor ingresado, indicar si tiene contenido.
+
+// let valor = prompt("Ingrese un valor:");
+
+// if (valor.length > 0) {
+// 	alert("Este valor tiene contenido");
+// }
+
+// 3)
+
+// Dado un número, indicar si ese número es mayor o menor o igual 
+// a 10.
+
+// let numero = prompt("Ingrese un número: ");
+
+// if (numero > 10) {
+// 	alert("El número es mayor a 10");
+// } else {
+// 	alert("El número es menor o igual a 10");
+// }
+
+// Dado un número, indicar si ese número es par o impar.
+
+// let numero = prompt("Ingrese un número");
+
+// if (numero%2 === 0 ) {
+// 	alert("El número es par");
+// } else {
+// 	alert("El número es impar");
+// }
+
+// Dados dos números, indicar cual es el mayor.
+
+// let numero1 = prompt("Ingrese un número");
+// let numero2 = prompt("Ingrese otro número");
+
+// if (numero1>numero2){
+// 	alert("El primer numero es mayor");
+// } else {
+// 	alert("El segundo número es mayor o igual");
+// }
+
+// Ante un valor ingresado, indicar si tiene o no contenido.
+
+// TAREA!
+
+// 4)
+
+// Dados dos números, indicar cual es mayor o si son iguales.
+
+// let numero1 = prompt("Ingrese un número");
+// let numero2 = prompt("Ingrese otro número");
+
+// if (numero1>numero2) {
+// 	alert("El primer número es mayor.");
+// } else if (numero1<numero2){
+// 	alert("El segundo número es mayor.");
+// } else {
+// 	alert("Ambos números son iguales.")
+// }
+
+// let numero1 = prompt("Ingrese un número");
+// let numero2 = prompt("Ingrese otro número");
+
+// if (numero1>numero2) {
+// 	alert("El primer número es mayor.");
+// }
+// if (numero1<numero2) {
+// 	alert("El segundo número es mayor.");
+// }
+// if (numero1===numero2) {
+// 	alert("Ambos números son iguales.");
+// }
+
+
+// Pedir las tres notas de un alumno y calcular su promedio, si es mayor
+// a 6, mostrar que aprobó, si es menor a 6, mostrar que desaprobó
+// y si es igual a 6, mostrar que está en condiciones de recuperación.
+
+// let notas1 = 1;
+// console.log(notas1);
+// let notas2 = 1;
+// console.log(notas2);
+// let notas3 = 1;
+// console.log(notas3);
+
+// let promedio = (notas1+notas2+notas3)/3;
+// console.log(promedio);
+
+// if (promedio>6) {
+// 	alert("Aprobado!, su promedio es de " + promedio)
+// } else if (promedio<6) {
+// 	alert("Desaprobado!, su promedio es de " + promedio);
+// } else {
+// 	alert("Recupera!, su promedio es de " + promedio)
+// }
+
+// Crear un algoritmo que pida al usuario el diámetro de una rueda
+// y su grosor (en metros) y realice las siguientes operaciones:
+
+// TAREA
+
+// a) Si el diámetro es superior a 1.4 debe mostrarse el mensaje
+//“La rueda es para un vehículo grande”. Si es menor o igual a 1.4
+// pero mayor que 0.8 debe mostrarse el mensaje “La rueda es para
+// un vehículo mediano”. Si no se cumplen ninguna de las condiciones
+// anteriores debe mostrarse por pantalla el mensaje “La rueda es 
+//para un vehículo pequeño”.
+
+// TAREA
+
+// Solicitar un numero de mes y mostrar el nombre del mes
+// correspondiente.
+
+// Solicitar un numero de día y mostrar el nombre del día
+
+
+// 5)
+
+// b) Si el diámetro es superior a 1.4 con un grosor inferior a 0.4,
+// ó si el diámetro es menor o igual a 1.4 pero mayor que 0.8, con un
+// grosor inferior a 0.25, deberá mostrarse el mensaje “El grosor para
+// esta rueda es inferior al recomendado”
+
+
+// 6)
+
+// Hacer ahora con Switch:
+// Pedir las tres notas de un alumno y calcular su promedio, si es mayor
+// a 6, mostrar que aprobó, si es menor a 6, mostrar que desaprobó
+// y si es igual a 6, mostrar que está en condiciones de recuperación.
+
+// TAREA
+
+// Hacer ahora con Switch:
+// Solicitar un numero de mes y mostrar el nombre del mes
+// correspondiente.
+
+let mes = prompt("Ingrese un número de mes.");
+
+switch (mes) {
+	case "1":
+		alert("Enero");
+		break;
+
+	case "2":
+		alert("Febrero");
+		break;
+
+	case "3":
+		alert("Marzo");
+		break;
+
+	case "4":
+		alert("Abril");
+		break;
+
+	case "5":
+		alert("Mayo");
+		break;
+
+	case "6":
+		alert("Junio");
+		break;
+
+	case "7":
+		alert("Julio");
+		break;
+
+	case "8":
+		alert("Agosto");
+		break;	
+
+	case "9":
+		alert("Septiembre");
+		break;
+
+	case "10":
+		alert("Octubre");
+		break;	
+		
+	case "11":
+		alert("Noviembre");
+		break;	
+
+	case "12":
+		alert("Diciembre");
+		break;
+
+	default:
+		alert("No hay ningún mes con ese número.")
+		break;
+}
+
+// Hacer ahora con Switch:
+// Solicitar un numero de día y mostrar el nombre del día
+
+// TAREA
+
+// Escribe un programa que responda a un usuario que quiere comprar
+// un helado en una conocida marca de comida rápida cuanto le costará
+// en función del topping que elija. Si no hay concidencias debe
+// informarlo.
+// El helado sin topping cuesta $100.
+// El topping de oreo cuesta $150.
+// El topping de KitKat cuesta $175.
+// El topping de brownie cuesta $175.
+
+// TAREA
+
+// Solicitar al usuario un modelo de auto que desea comprar e
+// informarle cuánto cuesta. Si no hay concidencias debe informarlo.
+
+// TAREA
+
+
+
